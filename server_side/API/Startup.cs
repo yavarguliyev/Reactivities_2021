d8df6f897acbdf871f8dec9993f6c91e15b5bc6b
yavarguliyev <yavarrg@code.edu.az>
2021-03-17 20:04:49 +0400

    security headers added

--- server_side/API/Startup.cs
+++ server_side/API/Startup.cs
@@ -46,10 +46,32 @@ namespace API
     {
       app.UseMiddleware<ExceptionMiddleware>();
 
+      app.UseXContentTypeOptions();
+      app.UseReferrerPolicy(opt => opt.NoReferrer());
+      app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
+      app.UseXfo(opt => opt.Deny());
+      app.UseCsp(opt => opt
+          .BlockAllMixedContent()
+          .StyleSources(s => s.Self().CustomSources("https://fonts.googleapis.com"))
+          .FontSources(s => s.Self().CustomSources("https://fonts.gstatic.com", "data:"))
+          .FormActions(s => s.Self())
+          .FrameAncestors(s => s.Self())
+          .ImageSources(s => s.Self().CustomSources("https://res.cloudinary.com"))
+          .ScriptSources(s => s.Self().CustomSources("sha256-jhwwwF3J2qFo8IPNQN+eH3TSp17EKnIcY9r8yUVArU0="))
+      );
+
       if (env.IsDevelopment())
       {
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+      }
+      else
+      {
+        app.Use(async (context, next) =>
+        {
+          context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000");
+          await next.Invoke();
+        });
       }
 
       // app.UseHttpsRedirection();
